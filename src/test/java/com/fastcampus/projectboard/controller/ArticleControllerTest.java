2a98d58b81f19700985ad5968135a9d3024015c9
changwook yang <ycw92325@gmail.com>
2023-01-16 03:11:22 +0900

    #18 - 게시판 뷰 기본골격 작성

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -30,7 +30,7 @@ class ArticleControllerTest {
         // WHen
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
